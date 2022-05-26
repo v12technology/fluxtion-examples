5a8be0512f995734d8615e3b718173f4ba64946e
greg higgins <greg.higgins@v12technology.com>
2022-05-26 10:52:06 +0100

    refactored to handle deltas. Use release version of fluxtion. Some simplifications

--- unplugged/part1/src/main/java/com/fluxtion/example/unplugged/part1/Main.java
+++ unplugged/part1/src/main/java/com/fluxtion/example/unplugged/part1/Main.java
@@ -3,25 +3,21 @@ package com.fluxtion.example.unplugged.part1;
 public class Main {
     public static void main(String[] args) {
         TradingCalculator tradingCalculator = new TradingCalculator();
-        //add listener
-        tradingCalculator.markToMarketListener(m -> System.out.println("Asset mark to market\t:" + m));
-        tradingCalculator.positionsListener(m -> System.out.println(   "Asset positions\t\t\t:" + m));
-        tradingCalculator.profitListener(d -> System.out.println(      "Total trading profit\t:" + d));
+        //add listeners for output
+        tradingCalculator.markToMarketConsumer(m -> System.out.println("Asset mark to market\t:" + m));
+        tradingCalculator.positionsConsumer(m -> System.out.println(   "Asset positions\t\t\t:" + m));
+        tradingCalculator.profitConsumer(d -> System.out.println(      "Total trading profit\t:" + d));
         //send trades and rates
-        System.out.println("sending trades and rates:");
-        tradingCalculator.priceUpdate(new PairPrice("EURUSD", 1.5));
+        tradingCalculator.processTrade(Trade.bought("EURUSD", 250d, 130d));
         tradingCalculator.processTrade(Trade.bought("EURUSD", 250d, 130d));
         tradingCalculator.processTrade(Trade.sold("EURCHF", 120d, 100d));
+        tradingCalculator.priceUpdate(new PairPrice("EURUSD", 1.5));
         tradingCalculator.priceUpdate(new PairPrice("USDCHF", 1.2));
-
-        System.out.println("\nset eur=usd rate parity:");
+        tradingCalculator.processTrade(Trade.bought("GBPJPY", 20d, 26000d));
         tradingCalculator.priceUpdate(new PairPrice("EURUSD", 1.0));
         //reset
-        System.out.println("\nresetting:");
         tradingCalculator.reset();
-
-        //more trades
-        System.out.println("\nsending trades and rates:");
+        //more trades after reset
         tradingCalculator.priceUpdate(new PairPrice("EURUSD", 1.5));
         tradingCalculator.priceUpdate(new PairPrice("GBPUSD", 1.25));
         tradingCalculator.priceUpdate(new PairPrice("USDJPY", 202));
--- unplugged/part1/src/main/java/com/fluxtion/example/unplugged/part1/TradingCalculator.java
+++ unplugged/part1/src/main/java/com/fluxtion/example/unplugged/part1/TradingCalculator.java
@@ -5,18 +5,12 @@ import com.fluxtion.compiler.SEPConfig;
 import com.fluxtion.example.unplugged.part1.Trade.AssetPrice;
 import com.fluxtion.example.unplugged.part1.Trade.TradeLeg;
 import com.fluxtion.runtime.EventProcessor;
-import com.fluxtion.runtime.annotations.OnEventHandler;
-import com.fluxtion.runtime.event.Signal;
 import com.fluxtion.runtime.stream.groupby.GroupBy;
 import com.fluxtion.runtime.stream.groupby.GroupBy.KeyValue;
 import com.fluxtion.runtime.stream.groupby.GroupByStreamed;
 import com.fluxtion.runtime.stream.helpers.Aggregates;
 import com.fluxtion.runtime.stream.helpers.Predicates;
-import lombok.EqualsAndHashCode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.DoubleConsumer;
@@ -24,65 +18,67 @@ import java.util.function.DoubleConsumer;
 import static com.fluxtion.compiler.builder.stream.EventFlow.subscribe;
 import static com.fluxtion.compiler.builder.stream.EventFlow.subscribeToSignal;
 
+/**
+ * Provides a Trading calculator service interface. An internal event processor is built that supports all the
+ * trading calculations. A service interface transforms method calls into events to make the processor easier
+ * to work with for clients.
+ *
+ * functionality:
+ * <ul>
+ *     <li>Process trades and calculation positions for individual asset</li>
+ *     <li>An FX trade has two legs, so one trade will create two position entries</li>
+ *     <li>Process market rates and calculate the value of an assets positions</li>
+ *     <li>The value is with reference to a base currency of USD</li>
+ *     <li>Listeners can register for positions, mark to market for each position and a global net profit aggregate</li>
+ *     <li>Only publish updates to a listener if a there is a change to the relevant subscription</li>
+ *     <li>Provide a reset control action that resets positions and market data</li>
+ *     <li></li>
+ * </ul>
+ */
 public class TradingCalculator {
 
     private final EventProcessor streamProcessor;
+    private static final String baseCurrency = "USD";
 
     public TradingCalculator() {
         streamProcessor = Fluxtion.interpret(this::buildProcessor);
         streamProcessor.init();
     }
 
-    public static KeyValue<String, Double> markToMarketPrice(KeyValue<String, Double> assetPosition, Map<String, Double> assetPriceMap) {
-        if (assetPosition == null || assetPriceMap == null) {
-            return null;
-        }
-        Double price = assetPriceMap.get(assetPosition.getKey());
-        if(price == null){
-            return null;
-        }
-        return new KeyValue<>(assetPosition.getKey(), price * assetPosition.getValue());
-    }
-
-    public static KeyValue<String, Double> markToMarketPosition(KeyValue<String, Double> assetPosition, Map<String, Double> assetPriceMap) {
-        if (assetPosition == null || assetPriceMap == null) {
-            return null;
-        }
-        Double price = assetPriceMap.getOrDefault(assetPosition.getKey(), Double.NaN);
-        return new KeyValue<>(assetPosition.getKey(), price * assetPosition.getValue());
-    }
-
-    public static double totalProfit(Map<String, Double> m) {
-        return m.values().stream().mapToDouble(Double::doubleValue).sum();
-    }
-
     public void processTrade(Trade trade) {
+        System.out.println("\nrcvd trade -> " + trade);
         streamProcessor.onEvent(trade);
         streamProcessor.publishSignal("publish");
     }
 
     public void priceUpdate(PairPrice price) {
+        System.out.println("\nrcvd price -> " + price);
         streamProcessor.onEvent(price);
         streamProcessor.publishSignal("publish");
     }
 
     public void reset() {
+        System.out.println("\nreset");
         streamProcessor.publishSignal("reset");
         streamProcessor.publishSignal("publish");
     }
 
-    public void markToMarketListener(Consumer<Map<String, Double>> listener) {
+    public void markToMarketConsumer(Consumer<Map<String, Double>> listener) {
         streamProcessor.addSink("mtm", listener);
     }
 
-    public void positionsListener(Consumer<Map<String, Double>> listener) {
+    public void positionsConsumer(Consumer<Map<String, Double>> listener) {
         streamProcessor.addSink("positions", listener);
     }
 
-    public void profitListener(DoubleConsumer listener){
+    public void profitConsumer(DoubleConsumer listener) {
         streamProcessor.addSink("profit", listener);
     }
 
+    /**
+     * Build the actual processing graph
+     *
+     */
     private void buildProcessor(SEPConfig config) {
         var resetTrigger = subscribeToSignal("reset");
         var publishTrigger = subscribeToSignal("publish");
@@ -93,77 +89,67 @@ public class TradingCalculator {
                 .resetTrigger(resetTrigger);
 
         var assetPriceMap = subscribe(PairPrice.class)
-                .flatMap(new ConvertToBasePrice("USD")::toCrossRate)
-                .groupBy(Trade.AssetPrice::id, Trade.AssetPrice::price, Aggregates.identity())
+                .map(TradingCalculator::toCrossRate)
+                .groupBy(Trade.AssetPrice::id, Trade.AssetPrice::price)
                 .resetTrigger(resetTrigger);
 
         var posDrivenMtmStream = assetPosition.map(GroupByStreamed::keyValue)
-                .map(TradingCalculator::markToMarketPosition, assetPriceMap.map(GroupBy::map));
+                .map(TradingCalculator::markToMarketPosition, assetPriceMap.map(GroupBy::map)).updateTrigger(assetPosition);
 
         var priceDrivenMtMStream = assetPriceMap.map(GroupByStreamed::keyValue)
                 .map(TradingCalculator::markToMarketPrice, assetPosition.map(GroupBy::map)).updateTrigger(assetPriceMap);
 
         //Mark to market to sink as a map
         var mtm = posDrivenMtmStream.merge(priceDrivenMtMStream)
-                .groupBy(KeyValue::getKey, KeyValue::getValueAsDouble, Aggregates.identity())
+                .groupBy(KeyValue::getKey, KeyValue::getValueAsDouble)
                 .resetTrigger(resetTrigger)
                 .map(GroupBy::map)
-                .defaultValue(Collections::emptyMap)
                 .updateTrigger(publishTrigger)
-//                .filter(Predicates.hasChangedFilter())
+                .filter(Predicates.hasMapChanged())
                 .sink("mtm");
 
         //Positions to sink as a map
         assetPosition.map(GroupBy::map)
-                .defaultValue(Collections::emptyMap)
                 .updateTrigger(publishTrigger)
-                .filter(Predicates.hasChangedFilter())
+                .filter(Predicates.hasMapChanged())
                 .sink("positions");
 
         //sum of mtm is profit
         mtm.mapToDouble(TradingCalculator::totalProfit)
-                .defaultValue(Double.NaN)
-                .resetTrigger(resetTrigger)
-                .updateTrigger(publishTrigger)
-//                .filter(Predicates.HAS_CHANGED_DOUBLE_FILTER)
+                .filter(Predicates.hasDoubleChanged())
                 .sink("profit");
     }
 
-    @EqualsAndHashCode
-    public static class ConvertToBasePrice {
-        private String baseCurrency;
-        private transient boolean hasPublished = false;
-
-        public ConvertToBasePrice() {
-            this("USD");
+    //Helper functions used during event processing
+    public static KeyValue<String, Double> markToMarketPrice(KeyValue<String, Double> assetPrice, Map<String, Double> assetPositionMap) {
+        if (assetPrice == null || assetPositionMap.get(assetPrice.getKey()) == null) {
+            return null;
         }
-
-        public ConvertToBasePrice(String baseCurrency) {
-            this.baseCurrency = baseCurrency;
-        }
-
-        public List<AssetPrice> toCrossRate(PairPrice pairPrice) {
-            List<AssetPrice> list = new ArrayList<>();
-            if (!hasPublished) {
-                list.add(new AssetPrice(baseCurrency, 1.0));
-            }
-            if (pairPrice.id().startsWith(baseCurrency)) {
-                list.add(new AssetPrice(pairPrice.id().substring(3), 1.0 / pairPrice.price()));
-            } else if (pairPrice.id().contains(baseCurrency)) {
-                list.add(new AssetPrice(pairPrice.id().substring(0, 3), pairPrice.price()));
-            }
-            hasPublished = true;
-            return list;
-        }
-
-        @OnEventHandler(filterString = "reset")
-        public void reset(Signal<Object> reset) {
-            hasPublished = false;
-        }
-
-        @OnEventHandler(filterString = "baseCurrency")
-        public void updateBaseCurrency(Signal<String> signal) {
-            this.baseCurrency = signal.getValue();
-        }
+        return new KeyValue<>(assetPrice.getKey(), assetPositionMap.get(assetPrice.getKey()) * assetPrice.getValue());
     }
+
+    public static KeyValue<String, Double> markToMarketPosition(KeyValue<String, Double> assetPosition, Map<String, Double> assetPriceMap) {
+        if (assetPosition == null) {
+            return null;
+        }
+        if (assetPosition.getKey().equals(baseCurrency)) {
+            return new KeyValue<>(assetPosition.getKey(), assetPosition.getValue());
+        }
+        if(assetPriceMap == null){
+            return new KeyValue<>(assetPosition.getKey(), Double.NaN);
+        }
+        return new KeyValue<>(assetPosition.getKey(), assetPriceMap.getOrDefault(assetPosition.getKey(), Double.NaN) * assetPosition.getValue());
+    }
+
+    public static double totalProfit(Map<String, Double> m) {
+        return m.values().stream().mapToDouble(Double::doubleValue).sum();
+    }
+
+    public static AssetPrice toCrossRate(PairPrice pairPrice) {
+        if (pairPrice.id().startsWith(baseCurrency)) {
+            return (new AssetPrice(pairPrice.id().substring(3), 1.0 / pairPrice.price()));
+        }
+        return (new AssetPrice(pairPrice.id().substring(0, 3), pairPrice.price()));
+    }
+
 }
