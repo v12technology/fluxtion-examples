0612611309dfcbf9104dd1f8394534e62b912acf
command-line <gregphiggins@gmail.com>
2023-02-24 17:15:30 +0000

    tidying up examples

--- topgun/src/main/java/com/fluxtion/example/topgun/Main.java
+++
@@ -1,9 +0,0 @@
-package com.fluxtion.example.topgun;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
-    public record Kill(String id) {}
-}
