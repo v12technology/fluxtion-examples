b1d959d3b1779e07262240031e8011f8dd9f1bdb
command-line <gregphiggins@gmail.com>
2023-03-07 17:09:45 +0000

    enabled LongLookup

--- cookbook-functional/src/main/java/com/fluxtion/example/cookbook_functional/statefulfilter/Main.java
+++ cookbook-functional/src/main/java/com/fluxtion/example/cookbook_functional/statefulfilter/Main.java
@@ -4,32 +4,30 @@ import com.fluxtion.compiler.Fluxtion;
 import com.fluxtion.compiler.builder.stream.EventFlow;
 import com.fluxtion.example.cookbook_functional.events.MarketUpdate;
 import com.fluxtion.runtime.EventProcessor;
-//import com.fluxtion.runtime.stream.lookup.LongLookupPredicate;
+import com.fluxtion.runtime.stream.lookup.LongLookupPredicate;
 
 import java.util.function.ToLongFunction;
 
 public class Main {
 
-
     public static void main(String[] args) {
-//        var eventProcessor = Fluxtion.interpret(c -> {
-//            EventFlow.subscribe(MarketUpdate.class)
-//                    .filterByProperty(
-//                            MarketUpdate::id,
-//                            LongLookupPredicate.buildPredicate("EURUSD", "marketRefData"))
-//                    .console("Filtered :{}");
-//
-//        });
-//
-//        eventProcessor.injectNamedInstance((ToLongFunction<String>)new MarketReferenceData()::toId, ToLongFunction.class, "marketRefData");
-//        eventProcessor.init();
-//        System.out.println("No filtering - ignore all MarketUpdate's");
-//        sendMarketEvents(eventProcessor);
-    }
+        var eventProcessor = Fluxtion.interpret(c -> {
+            EventFlow.subscribe(MarketUpdate.class)
+                    .filterByProperty(
+                            MarketUpdate::id,
+                            LongLookupPredicate.buildPredicate("EURUSD", "marketRefData"))
+                    .console("Filtered :{}");
 
-    @SuppressWarnings("unchecked")
-    private static <T> Class<T> generify(Class<?> cls) {
-        return (Class<T>)cls;
+        });
+
+        //cast required because of limitation of generics on class literals
+        eventProcessor.injectNamedInstance(
+                (ToLongFunction<String>) new MarketReferenceData()::toId,
+                ToLongFunction.class,
+                "marketRefData");
+        eventProcessor.init();
+        System.out.println("No filtering - ignore all MarketUpdate's");
+        sendMarketEvents(eventProcessor);
     }
 
     private static void sendMarketEvents(EventProcessor<?> processor) {
@@ -44,10 +42,10 @@ public class Main {
         processor.onEvent(new MarketUpdate(15, "USDGBP", 1.15));
     }
 
-    public static class MarketReferenceData{
+    public static class MarketReferenceData {
 
-        public long toId(String marketName){
-            return switch (marketName){
+        public long toId(String marketName) {
+            return switch (marketName) {
                 case "EURUSD" -> 10;
                 case "EURCHF" -> 11;
                 case "USDGBP" -> 15;
