01744d69c52e7b357f6761c72b49931abe12112b
command-line <gregphiggins@gmail.com>
2023-09-25 06:08:30 +0100

    added getting started tutorial 2 aot

---
+++ getting-started/tutorial3-lottery-auditlog/src/main/java/com/fluxtion/example/cookbook/lottery/nodes/TicketStoreNode.java
@@ -0,0 +1,76 @@
+package com.fluxtion.example.cookbook.lottery.nodes;
+
+import com.fluxtion.example.cookbook.lottery.api.Ticket;
+import com.fluxtion.example.cookbook.lottery.api.TicketStore;
+import com.fluxtion.runtime.annotations.ExportService;
+import com.fluxtion.runtime.annotations.NoPropagateFunction;
+import com.fluxtion.runtime.annotations.Start;
+import com.fluxtion.runtime.audit.EventLogNode;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class TicketStoreNode extends EventLogNode implements
+        Supplier<Ticket>,
+        @ExportService TicketStore {
+
+    private boolean storeOpen;
+    private Consumer<String> ticketSalesPublisher;
+    private Ticket ticket;
+
+    @Override
+    @NoPropagateFunction
+    public void setTicketSalesPublisher(Consumer<String> ticketSalesPublisher) {
+        this.ticketSalesPublisher = ticketSalesPublisher;
+    }
+
+    @Start
+    public void start() {
+        Objects.requireNonNull(ticketSalesPublisher, "must have a ticketSalesPublisher set");
+        storeOpen = false;
+        auditLog.info("ticketSalesPublisher", "valid")
+                .info("storeOpen", storeOpen);
+    }
+
+    @Override
+    public boolean buyTicket(Ticket ticket) {
+        if (ticket.number() < 9_99_99 | ticket.number() > 99_99_99) {
+            auditLog.info("rejectTicket", true)
+                    .info("badTicket", ticket)
+                    .info("invalidNumber", ticket.number());
+            ticketSalesPublisher.accept("invalid numbers " + ticket);
+            this.ticket = null;
+        } else if (storeOpen) {
+            ticketSalesPublisher.accept("good luck with " + ticket);
+            auditLog.info("ticketPurchased", ticket);
+            this.ticket = ticket;
+        } else {
+            ticketSalesPublisher.accept("store shut - no tickets can be bought");
+            auditLog.info("rejectTicket", true)
+                    .info("storeOpen", storeOpen);
+            this.ticket = null;
+        }
+        return this.ticket != null;
+    }
+
+    @Override
+    public Ticket get() {
+        return ticket;
+    }
+
+    @Override
+    @NoPropagateFunction
+    public void openStore() {
+        storeOpen = true;
+        auditLog.info("storeOpen", storeOpen);
+    }
+
+    @Override
+    @NoPropagateFunction
+    public void closeStore() {
+        storeOpen = false;
+        auditLog.info("storeOpen", storeOpen);
+    }
+}
