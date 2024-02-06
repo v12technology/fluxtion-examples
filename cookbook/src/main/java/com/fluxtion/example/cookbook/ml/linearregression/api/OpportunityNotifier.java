5f14a89b81cce9e73fc4a0c7efe1c374fa7c1fbc
greg <greg.h@vidarrcap.com>
2024-02-06 22:59:12 +0000

    tidy ml example

--- cookbook/src/main/java/com/fluxtion/example/cookbook/ml/linearregression/api/OpportunityNotifier.java
+++ cookbook/src/main/java/com/fluxtion/example/cookbook/ml/linearregression/api/OpportunityNotifier.java
@@ -3,7 +3,7 @@ package com.fluxtion.example.cookbook.ml.linearregression.api;
 import java.util.function.Consumer;
 
 public interface OpportunityNotifier {
-    default void setNotificationSink(Consumer<Object> notifierSink){}
-    default void setProfitTrigger(double profitTrigger){}
-    default void setEnableNotifications(boolean enableNotifications){}
+    void setNotificationSink(Consumer<Object> notifierSink);
+    void setProfitTrigger(double profitTrigger);
+    void setEnableNotifications(boolean enableNotifications);
 }
