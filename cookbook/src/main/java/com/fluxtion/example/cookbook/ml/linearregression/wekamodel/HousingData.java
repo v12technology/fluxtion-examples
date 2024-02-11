b9da0f5bd80da15af7b50916f52f53a4340abddd
greg <greg.h@vidarrcap.com>
2024-02-11 22:31:41 +0000

    more work on ml example

--- cookbook/src/main/java/com/fluxtion/example/cookbook/ml/linearregression/wekamodel/HousingData.java
+++ cookbook/src/main/java/com/fluxtion/example/cookbook/ml/linearregression/wekamodel/HousingData.java
@@ -2,19 +2,29 @@ package com.fluxtion.example.cookbook.ml.linearregression.wekamodel;
 
 import com.fluxtion.extension.csvcompiler.annotations.ColumnMapping;
 import com.fluxtion.extension.csvcompiler.annotations.CsvMarshaller;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@CsvMarshaller
+@CsvMarshaller(fluent = true)
 @Accessors(fluent = true)
 public class HousingData {
+    //input only
+    @ColumnMapping(outputField = false)
     private int Order;
+    @ColumnMapping(outputField = false, columnName = "Lot Frontage", defaultValue = "-1")
+    private int Lot_Frontage;
+    @ColumnMapping(outputField = false, columnName = "MS Zoning")
+    private String MS_Zoning;
+
+    //input and output
     private int PID;
     @ColumnMapping(columnName = "MS SubClass")
-    private String MS_SubClass;
+    private int MS_SubClass;
+
+    //derived
+    @ColumnMapping(optionalField = true)
+    private int Lot_Frontage_Squared;
+    @ColumnMapping(optionalField = true)
+    private int ms_zone_category;
 }
--- cookbook/src/main/java/com/fluxtion/example/cookbook/ml/linearregression/wekamodel/MainTest.java
+++ cookbook/src/main/java/com/fluxtion/example/cookbook/ml/linearregression/wekamodel/MainTest.java
@@ -1,11 +1,38 @@
 package com.fluxtion.example.cookbook.ml.linearregression.wekamodel;
 
-import com.fluxtion.example.cookbook.ml.linearregression.api.HouseSaleDetails;
 import com.fluxtion.extension.csvcompiler.RowMarshaller;
+import lombok.SneakyThrows;
 
-import java.io.StringWriter;
+import java.nio.file.Path;
 
 public class MainTest {
 
+    @SneakyThrows
+    public static void main(String[] args) {
+        RowMarshaller.transform(
+                HousingData.class,
+                Path.of("data/ml/linear_regression/AmesHousing.csv"),
+                Path.of("data/ml/linear_regression/PostProcess.csv"),
+                s -> s.filter(h -> h.Lot_Frontage() > 0)
+                        .map(MainTest::squareLotFrontage)
+                        .map(MainTest::ms_zone_to_category)
+                        .filter(h -> h.ms_zone_category() > 0));
+    }
 
+    public static HousingData squareLotFrontage(HousingData housingData) {
+        int lotFrontage = housingData.Lot_Frontage();
+        housingData.Lot_Frontage_Squared(lotFrontage * lotFrontage);
+        return housingData;
+    }
+
+    public static HousingData ms_zone_to_category(HousingData housingData) {
+        switch (housingData.MS_Zoning()) {
+            case "A" -> housingData.ms_zone_category(1);
+            case "FV" -> housingData.ms_zone_category(2);
+            case "RL" -> housingData.ms_zone_category(3);
+            case "RM" -> housingData.ms_zone_category(4);
+            default -> housingData.ms_zone_category(-1);
+        }
+        return housingData;
+    }
 }
