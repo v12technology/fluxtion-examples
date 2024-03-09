ab45a9d5d69621d068fca8a3bba855e9bc3ad025
command-line <gregphiggins@gmail.com>
2024-03-09 19:32:10 +0000

    added no NoTriggerReference example

---
+++ reference-examples/runtime-execution/src/main/java/com/fluxtion/example/reference/execution/NonTriggerReference.java
@@ -0,0 +1,52 @@
+package com.fluxtion.example.reference.execution;
+
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.runtime.annotations.NoTriggerReference;
+import com.fluxtion.runtime.annotations.OnEventHandler;
+import com.fluxtion.runtime.annotations.OnTrigger;
+
+public class NonTriggerReference {
+    public static class MyNode {
+        @OnEventHandler
+        public boolean handleStringEvent(String stringToProcess) {
+            System.out.println("MyNode::handleStringEvent received:" + stringToProcess);
+            return true;
+        }
+
+    }
+
+    public static class MyNode2 {
+        @OnEventHandler
+        public boolean handleIntEvent(int intToProcess) {
+            System.out.println("MyNode2::handleIntEvent received:" + intToProcess);
+            return true;
+        }
+    }
+
+
+    public static class Child {
+        private final MyNode myNode;
+        @NoTriggerReference
+        private final MyNode2 myNode2;
+
+        public Child(MyNode myNode, MyNode2 myNode2) {
+            this.myNode = myNode;
+            this.myNode2 = myNode2;
+        }
+
+
+        @OnTrigger
+        public boolean triggered() {
+            System.out.println("Child:triggered");
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        var processor = Fluxtion.interpret(new Child(new MyNode(), new MyNode2()));
+        processor.init();
+        processor.onEvent("test");
+        System.out.println();
+        processor.onEvent(200);
+    }
+}
