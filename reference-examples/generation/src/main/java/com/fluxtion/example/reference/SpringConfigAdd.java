168bb70b419835819caafe70e6e06df64e7eccc3
command-line <gregphiggins@gmail.com>
2024-03-17 17:19:02 +0000

    added some binding examples

---
+++ reference-examples/generation/src/main/java/com/fluxtion/example/reference/SpringConfigAdd.java
@@ -0,0 +1,39 @@
+package com.fluxtion.example.reference;
+
+import com.fluxtion.compiler.extern.spring.FluxtionSpring;
+import com.fluxtion.runtime.annotations.OnEventHandler;
+import com.fluxtion.runtime.annotations.OnTrigger;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringConfigAdd {
+
+    public static class MyNode {
+        @OnEventHandler
+        public boolean handleStringEvent(String stringToProcess) {
+            System.out.println("MyNode::received:" + stringToProcess);
+            return true;
+        }
+    }
+
+    public static class Root1 {
+        private final MyNode myNode;
+
+        public Root1(MyNode myNode) {
+            this.myNode = myNode;
+        }
+
+        @OnTrigger
+        public boolean trigger() {
+            System.out.println("Root1::triggered");
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        var context = new ClassPathXmlApplicationContext("com/fluxtion/example/reference/spring-example.xml");
+        var processor = FluxtionSpring.interpret(context);
+        processor.init();
+
+        processor.onEvent("TEST");
+    }
+}
