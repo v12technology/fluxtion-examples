b014a5796eb96f9c31417d39d93519bbe081df2e
command-line <gregphiggins@gmail.com>
2024-03-17 16:14:23 +0000

    added some binding examples

---
+++ reference-examples/generation/src/main/java/com/fluxtion/example/reference/CombineFunctionalAndImperative.java
@@ -0,0 +1,34 @@
+package com.fluxtion.example.reference;
+
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.compiler.builder.dataflow.DataFlow;
+import com.fluxtion.runtime.annotations.OnEventHandler;
+
+public class CombineFunctionalAndImperative {
+
+    public static String toUpper(String incoming){
+        return incoming.toUpperCase();
+    }
+
+    public static class MyNode {
+        @OnEventHandler
+        public boolean handleStringEvent(String stringToProcess) {
+            System.out.println("IMPERATIVE received:" + stringToProcess);
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        var processor = Fluxtion.interpret(cfg -> {
+            DataFlow.subscribe(String.class)
+                    .console("FUNCTIONAL input: '{}'")
+                    .map(CombineFunctionalAndImperative::toUpper)
+                    .console("FUNCTIONAL transformed: '{}'");
+
+            cfg.addNode(new MyNode());
+        });
+
+        processor.init();
+        processor.onEvent("hello world");
+    }
+}
