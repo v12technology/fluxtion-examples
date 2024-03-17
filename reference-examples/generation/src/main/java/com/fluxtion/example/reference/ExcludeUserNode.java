0e2df9ac36085f8dd3453b6dfc7293162d7a888b
command-line <gregphiggins@gmail.com>
2024-03-17 11:55:30 +0000

    added some binding examples

---
+++ reference-examples/generation/src/main/java/com/fluxtion/example/reference/ExcludeUserNode.java
@@ -0,0 +1,38 @@
+package com.fluxtion.example.reference;
+
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.runtime.annotations.OnEventHandler;
+import com.fluxtion.runtime.annotations.OnTrigger;
+import com.fluxtion.runtime.annotations.builder.ExcludeNode;
+
+public class ExcludeUserNode {
+
+    public static class MyNode {
+        @OnEventHandler
+        public boolean handleStringEvent(String stringToProcess) {
+            System.out.println("MyNode::received:" + stringToProcess);
+            return true;
+        }
+    }
+
+    @ExcludeNode
+    public static class Root1 {
+        private final MyNode myNode;
+
+        public Root1(MyNode myNode) {
+            this.myNode = myNode;
+        }
+
+        @OnTrigger
+        public boolean trigger() {
+            System.out.println("Root1::triggered");
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        var processor = Fluxtion.interpret(new Root1(new MyNode()));
+        processor.init();
+        processor.onEvent("TEST");
+    }
+}
