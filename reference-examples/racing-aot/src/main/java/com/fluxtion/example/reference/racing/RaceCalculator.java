043d5fdc20039fad2e4809d83bef88e20b336388
greg <greg.h@vidarrcap.com>
2024-03-19 07:45:43 +0000

    split racing example to interpreted and aot

---
+++ reference-examples/racing-aot/src/main/java/com/fluxtion/example/reference/racing/RaceCalculator.java
@@ -0,0 +1,90 @@
+package com.fluxtion.example.reference.racing;
+
+import com.fluxtion.runtime.annotations.ExportService;
+import com.fluxtion.runtime.annotations.Initialise;
+import com.fluxtion.runtime.annotations.OnEventHandler;
+import com.fluxtion.runtime.annotations.OnTrigger;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RaceCalculator {
+
+    public record RunnerStarted(long runnerId, Instant startTime) {
+        RunnerStarted(long runnerId, String time) {
+            this(runnerId, Instant.parse(time));
+        }
+    }
+
+    public record RunnerFinished(long runnerId, Instant finishTime) {
+        RunnerFinished(long runnerId, String time) {
+            this(runnerId, Instant.parse(time));
+        }
+    }
+
+    public interface ResultsPublisher {
+        void publishAllResults();
+    }
+
+    private record RunningRecord(long runnerId, Instant startTime, Instant finishTime) {
+        RunningRecord(long runnerId, Instant startTime) {
+            this(runnerId, startTime, startTime);
+        }
+
+        public String runDuration() {
+            Duration duration = Duration.between(startTime, finishTime);
+            return duration.toHoursPart() + ":" + duration.toMinutesPart() + ":" + duration.toSecondsPart();
+        }
+    }
+
+    @Getter
+    public static class RaceTimeTracker {
+
+        private final transient Map<Long, RunningRecord> raceTimeMap = new HashMap<>();
+        private RunningRecord latestFinisher;
+
+        @Initialise
+        public void init(){
+            raceTimeMap.clear();
+        }
+
+        @OnEventHandler(propagate = false)
+        public boolean runnerStarted(RunnerStarted runnerStarted) {
+            long runnerId = runnerStarted.runnerId();
+            raceTimeMap.put(runnerId, new RunningRecord(runnerId, runnerStarted.startTime()));
+            return false;
+        }
+
+        @OnEventHandler
+        public boolean runnerFinished(RunnerFinished runner) {
+            latestFinisher = raceTimeMap.computeIfPresent(
+                    runner.runnerId(),
+                    (id, startRecord) -> new RunningRecord(id, startRecord.startTime(), runner.finishTime()));
+            return true;
+        }
+    }
+
+    @RequiredArgsConstructor
+    public static class ResultsPublisherImpl implements @ExportService ResultsPublisher {
+
+        private final RaceTimeTracker raceTimeTracker;
+
+        @OnTrigger
+        public boolean sendIndividualRunnerResult(){
+            var raceRecord = raceTimeTracker.getLatestFinisher();
+            System.out.format("Crossed the line runner:%d time [%s]%n", raceRecord.runnerId(), raceRecord.runDuration());
+            return false;
+        }
+
+        @Override
+        public void publishAllResults() {
+            System.out.println("\nFINAL RESULTS");
+            raceTimeTracker.getRaceTimeMap().forEach((l, r) ->
+                    System.out.println("id:" + l + " time [" + r.runDuration() + "]"));
+        }
+    }
+}
