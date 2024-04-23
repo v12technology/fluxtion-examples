78e67ac38f43443605e7634e30e90f898dbecb87
command-line <gregphiggins@gmail.com>
2024-04-23 20:56:03 +0100

    added spring AOT example

---
+++ reference-examples/generation/src/main/java/com/fluxtion/example/reference/generation/genoutput/SpringExampleProcessor.java
@@ -0,0 +1,359 @@
+/*
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the Server Side Public License, version 1,
+* as published by MongoDB, Inc.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* Server Side License for more details.
+*
+* You should have received a copy of the Server Side Public License
+* along with this program.  If not, see
+*
+<http://www.mongodb.com/licensing/server-side-public-license>.
+*/
+package com.fluxtion.example.reference.generation.genoutput;
+
+import com.fluxtion.runtime.StaticEventProcessor;
+import com.fluxtion.runtime.lifecycle.BatchHandler;
+import com.fluxtion.runtime.lifecycle.Lifecycle;
+import com.fluxtion.runtime.EventProcessor;
+import com.fluxtion.runtime.callback.InternalEventProcessor;
+import com.fluxtion.example.reference.binding.SpringConfigAdd.MyNode;
+import com.fluxtion.example.reference.binding.SpringConfigAdd.Root1;
+import com.fluxtion.runtime.EventProcessorContext;
+import com.fluxtion.runtime.audit.Auditor;
+import com.fluxtion.runtime.audit.EventLogManager;
+import com.fluxtion.runtime.audit.NodeNameAuditor;
+import com.fluxtion.runtime.callback.CallbackDispatcherImpl;
+import com.fluxtion.runtime.callback.ExportFunctionAuditEvent;
+import com.fluxtion.runtime.event.Event;
+import com.fluxtion.runtime.input.EventFeed;
+import com.fluxtion.runtime.input.SubscriptionManagerNode;
+import com.fluxtion.runtime.node.ForkedTriggerTask;
+import com.fluxtion.runtime.node.MutableEventProcessorContext;
+import com.fluxtion.runtime.time.Clock;
+import com.fluxtion.runtime.time.ClockStrategy.ClockStrategyEvent;
+import java.util.Map;
+
+import java.util.IdentityHashMap;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+
+/**
+ *
+ *
+ * <pre>
+ * generation time                 : Not available
+ * eventProcessorGenerator version : 9.2.23
+ * api version                     : 9.2.23
+ * </pre>
+ *
+ * Event classes supported:
+ *
+ * <ul>
+ *   <li>com.fluxtion.runtime.time.ClockStrategy.ClockStrategyEvent
+ *   <li>java.lang.String
+ * </ul>
+ *
+ * @author Greg Higgins
+ */
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class SpringExampleProcessor
+    implements EventProcessor<SpringExampleProcessor>,
+        StaticEventProcessor,
+        InternalEventProcessor,
+        BatchHandler,
+        Lifecycle {
+
+  //Node declarations
+  private final CallbackDispatcherImpl callbackDispatcher = new CallbackDispatcherImpl();
+  public final MyNode myNode = new MyNode();
+  public final NodeNameAuditor nodeNameLookup = new NodeNameAuditor();
+  public final Root1 root1 = new Root1(myNode);
+  private final SubscriptionManagerNode subscriptionManager = new SubscriptionManagerNode();
+  private final MutableEventProcessorContext context =
+      new MutableEventProcessorContext(
+          nodeNameLookup, callbackDispatcher, subscriptionManager, callbackDispatcher);
+  public final Clock clock = new Clock();
+  private final ExportFunctionAuditEvent functionAudit = new ExportFunctionAuditEvent();
+  //Dirty flags
+  private boolean initCalled = false;
+  private boolean processing = false;
+  private boolean buffering = false;
+  private final IdentityHashMap<Object, BooleanSupplier> dirtyFlagSupplierMap =
+      new IdentityHashMap<>(1);
+  private final IdentityHashMap<Object, Consumer<Boolean>> dirtyFlagUpdateMap =
+      new IdentityHashMap<>(1);
+
+  private boolean isDirty_myNode = false;
+  //Forked declarations
+
+  //Filter constants
+
+  public SpringExampleProcessor(Map<Object, Object> contextMap) {
+    context.replaceMappings(contextMap);
+    //node auditors
+    initialiseAuditor(clock);
+    initialiseAuditor(nodeNameLookup);
+    subscriptionManager.setSubscribingEventProcessor(this);
+    context.setEventProcessorCallback(this);
+  }
+
+  public SpringExampleProcessor() {
+    this(null);
+  }
+
+  @Override
+  public void init() {
+    initCalled = true;
+    auditEvent(Lifecycle.LifecycleEvent.Init);
+    //initialise dirty lookup map
+    isDirty("test");
+    clock.init();
+    afterEvent();
+  }
+
+  @Override
+  public void start() {
+    if (!initCalled) {
+      throw new RuntimeException("init() must be called before start()");
+    }
+    processing = true;
+    auditEvent(Lifecycle.LifecycleEvent.Start);
+
+    afterEvent();
+    callbackDispatcher.dispatchQueuedCallbacks();
+    processing = false;
+  }
+
+  @Override
+  public void stop() {
+    if (!initCalled) {
+      throw new RuntimeException("init() must be called before stop()");
+    }
+    processing = true;
+    auditEvent(Lifecycle.LifecycleEvent.Stop);
+
+    afterEvent();
+    callbackDispatcher.dispatchQueuedCallbacks();
+    processing = false;
+  }
+
+  @Override
+  public void tearDown() {
+    initCalled = false;
+    auditEvent(Lifecycle.LifecycleEvent.TearDown);
+    nodeNameLookup.tearDown();
+    clock.tearDown();
+    subscriptionManager.tearDown();
+    afterEvent();
+  }
+
+  @Override
+  public void setContextParameterMap(Map<Object, Object> newContextMapping) {
+    context.replaceMappings(newContextMapping);
+  }
+
+  @Override
+  public void addContextParameter(Object key, Object value) {
+    context.addMapping(key, value);
+  }
+
+  //EVENT DISPATCH - START
+  @Override
+  public void onEvent(Object event) {
+    if (buffering) {
+      triggerCalculation();
+    }
+    if (processing) {
+      callbackDispatcher.processReentrantEvent(event);
+    } else {
+      processing = true;
+      onEventInternal(event);
+      callbackDispatcher.dispatchQueuedCallbacks();
+      processing = false;
+    }
+  }
+
+  @Override
+  public void onEventInternal(Object event) {
+    if (event instanceof com.fluxtion.runtime.time.ClockStrategy.ClockStrategyEvent) {
+      ClockStrategyEvent typedEvent = (ClockStrategyEvent) event;
+      handleEvent(typedEvent);
+    } else if (event instanceof java.lang.String) {
+      String typedEvent = (String) event;
+      handleEvent(typedEvent);
+    }
+  }
+
+  public void handleEvent(ClockStrategyEvent typedEvent) {
+    auditEvent(typedEvent);
+    //Default, no filter methods
+    clock.setClockStrategy(typedEvent);
+    afterEvent();
+  }
+
+  public void handleEvent(String typedEvent) {
+    auditEvent(typedEvent);
+    //Default, no filter methods
+    isDirty_myNode = myNode.handleStringEvent(typedEvent);
+    if (guardCheck_root1()) {
+      root1.trigger();
+    }
+    afterEvent();
+  }
+  //EVENT DISPATCH - END
+
+  public void bufferEvent(Object event) {
+    buffering = true;
+    if (event instanceof com.fluxtion.runtime.time.ClockStrategy.ClockStrategyEvent) {
+      ClockStrategyEvent typedEvent = (ClockStrategyEvent) event;
+      auditEvent(typedEvent);
+      clock.setClockStrategy(typedEvent);
+    } else if (event instanceof java.lang.String) {
+      String typedEvent = (String) event;
+      auditEvent(typedEvent);
+      isDirty_myNode = myNode.handleStringEvent(typedEvent);
+    }
+  }
+
+  public void triggerCalculation() {
+    buffering = false;
+    String typedEvent = "No event information - buffered dispatch";
+    if (guardCheck_root1()) {
+      root1.trigger();
+    }
+    afterEvent();
+  }
+
+  private void auditEvent(Object typedEvent) {
+    clock.eventReceived(typedEvent);
+    nodeNameLookup.eventReceived(typedEvent);
+  }
+
+  private void auditEvent(Event typedEvent) {
+    clock.eventReceived(typedEvent);
+    nodeNameLookup.eventReceived(typedEvent);
+  }
+
+  private void initialiseAuditor(Auditor auditor) {
+    auditor.init();
+    auditor.nodeRegistered(myNode, "myNode");
+    auditor.nodeRegistered(root1, "root1");
+    auditor.nodeRegistered(callbackDispatcher, "callbackDispatcher");
+    auditor.nodeRegistered(subscriptionManager, "subscriptionManager");
+    auditor.nodeRegistered(context, "context");
+  }
+
+  private void beforeServiceCall(String functionDescription) {
+    functionAudit.setFunctionDescription(functionDescription);
+    auditEvent(functionAudit);
+    if (buffering) {
+      triggerCalculation();
+    }
+    processing = true;
+  }
+
+  private void afterServiceCall() {
+    afterEvent();
+    callbackDispatcher.dispatchQueuedCallbacks();
+    processing = false;
+  }
+
+  private void afterEvent() {
+
+    clock.processingComplete();
+    nodeNameLookup.processingComplete();
+    isDirty_myNode = false;
+  }
+
+  @Override
+  public void batchPause() {
+    auditEvent(Lifecycle.LifecycleEvent.BatchPause);
+    processing = true;
+
+    afterEvent();
+    callbackDispatcher.dispatchQueuedCallbacks();
+    processing = false;
+  }
+
+  @Override
+  public void batchEnd() {
+    auditEvent(Lifecycle.LifecycleEvent.BatchEnd);
+    processing = true;
+
+    afterEvent();
+    callbackDispatcher.dispatchQueuedCallbacks();
+    processing = false;
+  }
+
+  @Override
+  public boolean isDirty(Object node) {
+    return dirtySupplier(node).getAsBoolean();
+  }
+
+  @Override
+  public BooleanSupplier dirtySupplier(Object node) {
+    if (dirtyFlagSupplierMap.isEmpty()) {
+      dirtyFlagSupplierMap.put(myNode, () -> isDirty_myNode);
+    }
+    return dirtyFlagSupplierMap.getOrDefault(node, StaticEventProcessor.ALWAYS_FALSE);
+  }
+
+  @Override
+  public void setDirty(Object node, boolean dirtyFlag) {
+    if (dirtyFlagUpdateMap.isEmpty()) {
+      dirtyFlagUpdateMap.put(myNode, (b) -> isDirty_myNode = b);
+    }
+    dirtyFlagUpdateMap.get(node).accept(dirtyFlag);
+  }
+
+  private boolean guardCheck_root1() {
+    return isDirty_myNode;
+  }
+
+  @Override
+  public <T> T getNodeById(String id) throws NoSuchFieldException {
+    return nodeNameLookup.getInstanceById(id);
+  }
+
+  @Override
+  public <A extends Auditor> A getAuditorById(String id)
+      throws NoSuchFieldException, IllegalAccessException {
+    return (A) this.getClass().getField(id).get(this);
+  }
+
+  @Override
+  public void addEventFeed(EventFeed eventProcessorFeed) {
+    subscriptionManager.addEventProcessorFeed(eventProcessorFeed);
+  }
+
+  @Override
+  public void removeEventFeed(EventFeed eventProcessorFeed) {
+    subscriptionManager.removeEventProcessorFeed(eventProcessorFeed);
+  }
+
+  @Override
+  public SpringExampleProcessor newInstance() {
+    return new SpringExampleProcessor();
+  }
+
+  @Override
+  public SpringExampleProcessor newInstance(Map<Object, Object> contextMap) {
+    return new SpringExampleProcessor();
+  }
+
+  @Override
+  public String getLastAuditLogRecord() {
+    try {
+      EventLogManager eventLogManager =
+          (EventLogManager) this.getClass().getField(EventLogManager.NODE_NAME).get(this);
+      return eventLogManager.lastRecordAsString();
+    } catch (Throwable e) {
+      return "";
+    }
+  }
+}
