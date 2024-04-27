eaf7d10ca35044ba451298e7da2586b296002a2e
command-line <gregphiggins@gmail.com>
2024-04-27 11:18:55 +0100

    query and control examples

---
+++ reference-examples/integration/src/main/java/com/fluxtion/example/reference/integration/AuditLookupExample.java
@@ -0,0 +1,41 @@
+package com.fluxtion.example.reference.integration;
+
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.compiler.builder.dataflow.DataFlow;
+import com.fluxtion.runtime.audit.Auditor;
+
+public class AuditLookupExample {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        var processor = Fluxtion.interpret(c -> {
+            //ADDING A NAMED AUDITOR
+            c.addAuditor(new MyAuditor(), "myAuditor");
+            DataFlow.subscribe(String.class);
+        });
+        processor.init();
+        processor.onEvent("A");
+        processor.onEvent("B");
+        processor.onEvent("C");
+
+        //LOOKUP AUDITOR BY NAME
+        MyAuditor myAuditor = processor.getAuditorById("myAuditor");
+        System.out.println("\nMyAuditor::invocationCount " + myAuditor.getInvocationCount());
+    }
+
+    public static class MyAuditor implements Auditor {
+        private int invocationCount = 0;
+
+        @Override
+        public void nodeRegistered(Object node, String nodeName) {
+        }
+
+        @Override
+        public void eventReceived(Object event) {
+            System.out.println("MyAuditor::eventReceived " + event);
+            invocationCount++;
+        }
+
+        public int getInvocationCount() {
+            return invocationCount;
+        }
+    }
+}
--- reference-examples/integration/src/main/java/com/fluxtion/example/reference/integration/GetFlowNodeByIdExample.java
+++ reference-examples/integration/src/main/java/com/fluxtion/example/reference/integration/GetFlowNodeByIdExample.java
@@ -12,8 +12,9 @@ public class GetFlowNodeByIdExample {
         var processor = Fluxtion.interpret(c ->{
             DataFlow.subscribe(String.class)
                     .filter(s -> s.equalsIgnoreCase("monday"))
-                    .mapToInt(Mappers.count())
-                    .id("MondayChecker")//this makes the wrapped value accessible via the id
+                    //ID START - this makes the wrapped value accessible via the id
+                    .mapToInt(Mappers.count()).id("MondayChecker")
+                    //ID END
                     .console("Monday is triggered");
         });
         processor.init();
