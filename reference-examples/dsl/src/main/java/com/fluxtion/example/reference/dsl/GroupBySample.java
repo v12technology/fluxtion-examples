6073a9cd8192d2a6cbea7745ebe1f5b91243d82e
command-line <gregphiggins@gmail.com>
2024-05-04 22:24:09 +0100

    adding dsl aggregate and groupBy examples

--- reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/GroupBySample.java
+++ reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/GroupBySample.java
@@ -3,9 +3,11 @@ package com.fluxtion.example.reference.dsl;
 import com.fluxtion.compiler.EventProcessorConfig;
 import com.fluxtion.compiler.Fluxtion;
 import com.fluxtion.compiler.builder.dataflow.DataFlow;
+import com.fluxtion.runtime.dataflow.groupby.GroupBy;
+import com.fluxtion.runtime.dataflow.helpers.Aggregates;
 import com.fluxtion.runtime.dataflow.helpers.Collectors;
 
-public class AggregateSample {
+public class GroupBySample {
 
     public record ResetList() {
     }
@@ -13,29 +15,25 @@ public class AggregateSample {
     public static void buildGraph(EventProcessorConfig processorConfig) {
         var resetSignal = DataFlow.subscribe(ResetList.class).console("\n--- RESET ---");
 
-        DataFlow.subscribe(String.class)
-                .aggregate(Collectors.listFactory(3))
+        DataFlow.subscribe(Integer.class)
+                .groupBy(i -> i % 2 == 0 ? "evens" : "odds", Aggregates.countFactory())
                 .resetTrigger(resetSignal)
-                .console("ROLLING list: {}");
+                .map(GroupBy::toMap)
+                .console("ODD/EVEN map:{}");
     }
 
 
     public static void main(String[] args) {
-        var processor = Fluxtion.interpret(AggregateSample::buildGraph);
+        var processor = Fluxtion.interpret(GroupBySample::buildGraph);
         processor.init();
-        processor.onEvent("A");
-        processor.onEvent("B");
-        processor.onEvent("C");
-        processor.onEvent("D");
-        processor.onEvent("E");
+        processor.onEvent(1);
+        processor.onEvent(2);
 
         processor.onEvent(new ResetList());
-        processor.onEvent("P");
-        processor.onEvent("Q");
-        processor.onEvent("R");
+        processor.onEvent(5);
+        processor.onEvent(7);
 
         processor.onEvent(new ResetList());
-        processor.onEvent("XX");
-        processor.onEvent("YY");
+        processor.onEvent(2);
     }
 }
