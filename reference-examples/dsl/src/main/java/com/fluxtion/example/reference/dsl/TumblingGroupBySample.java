346e0136f7ac556a52898163a13ea0b8748e3730
command-line <gregphiggins@gmail.com>
2024-05-05 19:02:18 +0100

    updating dsl examples, adding groupBy examples

---
+++ reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/TumblingGroupBySample.java
@@ -0,0 +1,40 @@
+package com.fluxtion.example.reference.dsl;
+
+import com.fluxtion.compiler.EventProcessorConfig;
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.compiler.builder.dataflow.DataFlow;
+import com.fluxtion.runtime.dataflow.groupby.GroupBy;
+import com.fluxtion.runtime.dataflow.helpers.Aggregates;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TumblingGroupBySample {
+    public record Trade(String symbol, int amountTraded) {}
+    private static String[] symbols = new String[]{"GOOG", "AMZN", "MSFT", "TKM"};
+
+    public static void buildGraph(EventProcessorConfig processorConfig) {
+        DataFlow.subscribe(Trade.class)
+                .groupByTumbling(Trade::symbol, Trade::amountTraded, Aggregates.intSumFactory(), 250)
+                .map(GroupBy::toMap)
+                .console("Trade volume for last 250 millis:{} time:%e");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var processor = Fluxtion.interpret(TumblingGroupBySample::buildGraph);
+        processor.init();
+        Random rand = new Random();
+
+        try (ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor()) {
+            executor.scheduleAtFixedRate(
+                    () -> {
+                        processor.onEvent("tick");
+                        processor.onEvent(new Trade(symbols[rand.nextInt(symbols.length)], rand.nextInt(100)));
+                    },
+                    10,10, TimeUnit.MILLISECONDS);
+            Thread.sleep(4_000);
+        }
+    }
+}
