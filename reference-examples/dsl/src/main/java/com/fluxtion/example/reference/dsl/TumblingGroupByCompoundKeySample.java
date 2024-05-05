95b19e808a9eae99d0530687cac9f1c3a9c87c52
command-line <gregphiggins@gmail.com>
2024-05-06 00:02:09 +0100

    updating dsl examples, adding groupBy examples

---
+++ reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/TumblingGroupByCompoundKeySample.java
@@ -0,0 +1,54 @@
+package com.fluxtion.example.reference.dsl;
+
+import com.fluxtion.compiler.EventProcessorConfig;
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.compiler.builder.dataflow.DataFlow;
+import com.fluxtion.runtime.dataflow.groupby.GroupBy;
+import com.fluxtion.runtime.dataflow.groupby.GroupByKey;
+import com.fluxtion.runtime.dataflow.helpers.Aggregates;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TumblingGroupByCompoundKeySample {
+    public record Trade(String symbol, String client, int amountTraded) {}
+    private static String[] symbols = new String[]{"GOOG", "AMZN", "MSFT", "TKM"};
+    private static String[] clients = new String[]{"client_A", "client_B", "client_D", "client_E"};
+
+    public static void buildGraph(EventProcessorConfig processorConfig) {
+        DataFlow.subscribe(Trade.class)
+                .groupByTumbling(
+                        GroupByKey.build(Trade::client, Trade::symbol),
+                        Trade::amountTraded,
+                        Aggregates.intSumFactory(),
+                        250)
+                .map(TumblingGroupByCompoundKeySample::formatGroupBy)
+                .console("Trade volume tumbling per 250 millis by client and symbol time:%e:\n{}----------------------\n");
+    }
+
+    private static <T> String formatGroupBy(GroupBy<GroupByKey<T>, Integer> groupBy) {
+        Map<GroupByKey<T>, Integer> groupByMap = groupBy.toMap();
+        StringBuilder stringBuilder = new StringBuilder();
+        groupByMap.forEach((k, v) -> stringBuilder.append(k.getKey() + ": " + v + "\n"));
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var processor = Fluxtion.interpret(TumblingGroupByCompoundKeySample::buildGraph);
+        processor.init();
+        Random rand = new Random();
+
+        try (ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor()) {
+            executor.scheduleAtFixedRate(
+                    () -> {
+                        processor.onEvent("tick");
+                        processor.onEvent(new Trade(symbols[rand.nextInt(symbols.length)], clients[rand.nextInt(clients.length)], rand.nextInt(100)));
+                    },
+                    10,10, TimeUnit.MILLISECONDS);
+            Thread.sleep(4_000);
+        }
+    }
+}
