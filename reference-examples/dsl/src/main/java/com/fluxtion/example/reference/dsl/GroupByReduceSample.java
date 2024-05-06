3a98fc8e5f8658e910014e703a7a62b7703ecdbc
command-line <gregphiggins@gmail.com>
2024-05-06 19:24:14 +0100

    update examples to fluxtion version 9.3.0

--- reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/GroupByReduceSample.java
+++ reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/GroupByReduceSample.java
@@ -3,31 +3,25 @@ package com.fluxtion.example.reference.dsl;
 import com.fluxtion.compiler.EventProcessorConfig;
 import com.fluxtion.compiler.Fluxtion;
 import com.fluxtion.compiler.builder.dataflow.DataFlow;
-import com.fluxtion.runtime.dataflow.groupby.GroupBy;
+import com.fluxtion.runtime.dataflow.helpers.Aggregates;
 
-public class GroupByToListSample {
-
-    public record ResetList() {
-    }
+public class GroupByReduceSample {
 
     public static void buildGraph(EventProcessorConfig processorConfig) {
-        var resetSignal = DataFlow.subscribe(ResetList.class).console("\n--- RESET ---");
-
         DataFlow.subscribe(Integer.class)
-                .groupByToList(i -> i % 2 == 0 ? "evens" : "odds")
-                .resetTrigger(resetSignal)
-                .map(GroupBy::toMap)
-                .console("ODD/EVEN map:{}");
+                .groupBy(i -> i % 2 == 0 ? "evens" : "odds", Aggregates.intSumFactory())
+                .console("ODD/EVEN sum:{}")
+                .reduceValues(Aggregates.intSumFactory())
+                .console("REDUCED sum:{}\n");
     }
 
     public static void main(String[] args) {
-        var processor = Fluxtion.interpret(GroupByToListSample::buildGraph);
+        var processor = Fluxtion.interpret(GroupByReduceSample::buildGraph);
         processor.init();
         processor.onEvent(1);
         processor.onEvent(2);
         processor.onEvent(5);
         processor.onEvent(7);
         processor.onEvent(2);
-        processor.onEvent(new ResetList());
     }
 }
