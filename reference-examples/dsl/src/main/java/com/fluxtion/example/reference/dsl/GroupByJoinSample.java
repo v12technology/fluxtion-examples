80779b9c95fc015d00c146556c82c7a02cf0b5f7
command-line <gregphiggins@gmail.com>
2024-05-06 22:38:49 +0100

    update examples to fluxtion version 9.3.0

---
+++ reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/GroupByJoinSample.java
@@ -0,0 +1,51 @@
+package com.fluxtion.example.reference.dsl;
+
+import com.fluxtion.compiler.EventProcessorConfig;
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.compiler.builder.dataflow.DataFlow;
+import com.fluxtion.compiler.builder.dataflow.JoinFlowBuilder;
+import com.fluxtion.runtime.dataflow.groupby.GroupBy;
+import com.fluxtion.runtime.dataflow.helpers.Tuples;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GroupByJoinSample {
+
+    public record Pupil(int year, String school, String name){}
+    public record School(String name){}
+
+    public static void buildGraph(EventProcessorConfig processorConfig) {
+
+        var pupils = DataFlow.subscribe(Pupil.class).groupByToList(Pupil::school);
+        var schools = DataFlow.subscribe(School.class).groupBy(School::name);
+
+        JoinFlowBuilder.innerJoin(schools, pupils)
+                .mapValues(Tuples.mapTuple(GroupByJoinSample::prettyPrint))
+                .map(GroupBy::toMap)
+                .console();
+    }
+
+    private static String prettyPrint(School schoolName, List<Pupil> pupils) {
+        return pupils.stream().map(Pupil::name).collect(Collectors.joining(",", "pupils[", "] ") );
+    }
+
+
+    public static void main(String[] args) {
+        var processor = Fluxtion.interpret(GroupByJoinSample::buildGraph);
+        processor.init();
+
+        //register some schools
+        processor.onEvent(new School("RGS"));
+        processor.onEvent(new School("Belles"));
+
+        //register some pupils
+        processor.onEvent(new Pupil(2015, "RGS", "Bob"));
+        processor.onEvent(new Pupil(2013, "RGS", "Ashkay"));
+        processor.onEvent(new Pupil(2013, "Belles", "Channing"));
+        processor.onEvent(new Pupil(2013, "RGS", "Chelsea"));
+        processor.onEvent(new Pupil(2013, "Belles", "Tamsin"));
+        processor.onEvent(new Pupil(2013, "Belles", "Ayola"));
+        processor.onEvent(new Pupil(2015, "Belles", "Sunita"));
+    }
+}
