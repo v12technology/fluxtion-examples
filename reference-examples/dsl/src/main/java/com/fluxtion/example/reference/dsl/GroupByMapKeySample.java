3855ca54b44d0fe53c93071db695b3b53d180f97
command-line <gregphiggins@gmail.com>
2024-05-06 08:42:04 +0100

    updating dsl examples, adding groupBy examples

---
+++ reference-examples/dsl/src/main/java/com/fluxtion/example/reference/dsl/GroupByMapKeySample.java
@@ -0,0 +1,36 @@
+package com.fluxtion.example.reference.dsl;
+
+import com.fluxtion.compiler.EventProcessorConfig;
+import com.fluxtion.compiler.Fluxtion;
+import com.fluxtion.compiler.builder.dataflow.DataFlow;
+import com.fluxtion.runtime.dataflow.groupby.GroupBy;
+import com.fluxtion.runtime.dataflow.groupby.GroupByKey;
+import com.fluxtion.runtime.dataflow.helpers.Aggregates;
+
+import java.util.Map;
+
+public class GroupByMapKeySample {
+
+    public record Pupil(int year, String sex, String name){}
+
+    public static void buildGraph(EventProcessorConfig processorConfig) {
+        DataFlow.subscribe(Pupil.class)
+                .groupByFieldsAggregate(Aggregates.countFactory(), Pupil::year, Pupil::sex)
+                .mapKeys(GroupByKey::getKey)//MAPS KEYS
+                .map(GroupBy::toMap)
+                .console("{}\n----");
+    }
+
+    public static void main(String[] args) {
+        var processor = Fluxtion.interpret(GroupByMapKeySample::buildGraph);
+        processor.init();
+
+        processor.onEvent(new Pupil(2015, "Female", "Bob"));
+        processor.onEvent(new Pupil(2013, "Male", "Ashkay"));
+        processor.onEvent(new Pupil(2013, "Male", "Channing"));
+        processor.onEvent(new Pupil(2013, "Female", "Chelsea"));
+        processor.onEvent(new Pupil(2013, "Female", "Tamsin"));
+        processor.onEvent(new Pupil(2013, "Female", "Ayola"));
+        processor.onEvent(new Pupil(2015, "Female", "Sunita"));
+    }
+}
