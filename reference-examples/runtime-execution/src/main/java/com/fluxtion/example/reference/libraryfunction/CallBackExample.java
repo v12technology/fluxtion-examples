84deb22cb93b1155b7685b5224d4132661979002
greg <greg.h@vidarrcap.com>
2024-05-21 10:36:54 +0100

    formatting

--- reference-examples/runtime-execution/src/main/java/com/fluxtion/example/reference/libraryfunction/CallBackExample.java
+++ reference-examples/runtime-execution/src/main/java/com/fluxtion/example/reference/libraryfunction/CallBackExample.java
@@ -2,33 +2,57 @@ package com.fluxtion.example.reference.libraryfunction;
 
 import com.fluxtion.compiler.Fluxtion;
 import com.fluxtion.runtime.annotations.OnEventHandler;
+import com.fluxtion.runtime.annotations.OnTrigger;
 import com.fluxtion.runtime.annotations.builder.Inject;
 import com.fluxtion.runtime.callback.EventDispatcher;
+import lombok.Data;
 
 public class CallBackExample {
-    public static class MyCallbackNode{
+    public static class MyCallbackNode {
 
         @Inject
         public EventDispatcher eventDispatcher;
 
         @OnEventHandler
         public boolean processString(String event) {
+            System.out.println("MyCallbackNode::processString - " + event);
             for (String item : event.split(",")) {
                 eventDispatcher.processAsNewEventCycle(Integer.parseInt(item));
             }
-            return false;
+            return true;
         }
 
         @OnEventHandler
         public boolean processInteger(Integer event) {
-            System.out.println("received event: " + event);
+            System.out.println("MyCallbackNode::processInteger - " + event);
+            return false;
+        }
+
+    }
+
+    @Data
+    public static class IntegerHandler {
+
+        private final MyCallbackNode myCallbackNode;
+
+        @OnEventHandler
+        public boolean processInteger(Integer event) {
+            System.out.println("IntegerHandler::processInteger - " + event + "\n");
+            return true;
+        }
+
+        @OnTrigger
+        public boolean triggered() {
+            System.out.println("IntegerHandler::triggered\n");
             return false;
         }
 
     }
 
     public static void main(String[] args) {
-        var processor = Fluxtion.interpret(new MyCallbackNode());
+        MyCallbackNode myCallbackNode = new MyCallbackNode();
+        IntegerHandler intHandler = new IntegerHandler(myCallbackNode);
+        var processor = Fluxtion.interpret(intHandler);
         processor.init();
 
         processor.onEvent("20,45,89");
